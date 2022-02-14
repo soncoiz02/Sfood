cd7847461264651a8ea93940c6f4390910e16f46
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2022-02-14 15:41:24 +0700

    private router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Routes as Switch, Route, useLocation } from 'react-router-dom'
+import { Routes as Switch, Route } from 'react-router-dom'
 import Cart from '../page/Cart/Cart'
+import Checkout from '../page/Checkout/Checkout'
 import Detail from '../page/Detail/Detail'
 import Home from '../page/Home/Home'
 import Login from '../page/Login/Login'
 import Menu from '../page/Menu/Menu'
 import Products from '../page/Products/Products'
+import RequireAuth from './RequireAuth'
 
 const Router = () => {
     return (
@@ -19,8 +21,11 @@ const Router = () => {
                 <Route path="" element={<Detail />} />
                 <Route path=":id" element={<Detail />} />
             </Route>
+            <Route element={<RequireAuth />} >
+                <Route path='/checkout' element={<Checkout />} />
+                <Route path="/cart" element={<Cart />} />
+            </Route>
             <Route path="/login" element={<Login />} />
-            <Route path="/cart" element={<Cart />} />
         </Switch>
     )
 }
